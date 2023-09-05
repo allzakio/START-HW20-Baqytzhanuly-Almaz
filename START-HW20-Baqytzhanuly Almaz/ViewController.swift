0c5bd676a91776e7cc9b9c7ee9a5b84b2f1a5a23
allz <bakhytzhanov.7@gmail.com>
2023-09-05 15:59:49 +0600

    Initial Commit

---
+++ START-HW20-Baqytzhanuly Almaz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  START-HW20-Baqytzhanuly Almaz
+//
+//  Created by allz on 9/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
